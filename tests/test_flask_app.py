cd51c536d66b7c5b9db63e4e33790de96b4be3aa
abhishekla311 <abhishekla311@gmail.com>
2026-07-07 10:42:34 -0700

    fix: bypass load_dotenv override on github runner

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -1,6 +1,7 @@
 import unittest
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from flask_app.app import app
-
 class FlaskAppTests(unittest.TestCase):
 
     @classmethod
